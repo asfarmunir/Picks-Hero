c79223c7c28e15d4a9c82c4387461f9e257f7f4a
asfarmunir <asfarmunir12345@gmail.com>
2024-08-08 21:11:51 +0500

    collapsible sidebar added

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
@@ -68,102 +68,110 @@ const navlinks = [
 ];
 
 const Sidebar = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const pathname = usePathname();
-  console.log(pathname);
+
+  const handleCollapseToggle = () => {
+    setIsCollapsed(!isCollapsed);
+  };
 
   return (
     <>
-      <div className=" relative bg-primary border-r border-gray-800 h-full w-[22%] 2xl:w-[24%] min-h-svh hidden lg:flex gap-7 2xl:gap-10 flex-col p-4">
+      <div
+        className={`relative bg-primary border-r border-gray-800 h-full  transition-all duration-300 
+          ${
+            isCollapsed ? "w-[5%] lg:w-[6%]" : "w-[22%] ease-in-out 2xl:w-[24%]"
+          } 
+          min-h-screen hidden lg:flex flex-col gap-7 2xl:gap-10 p-4`}
+      >
         <Image
-          src="/images/logo.png"
+          src={!isCollapsed ? "/images/logo.png" : "/icons/logo.png"}
           alt="logo"
           width={170}
           height={170}
+          className={` ${isCollapsed && "w-[30px] h-[30px] mx-auto"} `}
           priority
         />
-        <div className="flex items-center gap-3">
-          <Image
-            src="/images/hello.png"
-            alt="Client"
-            width={46}
-            height={46}
-            className=" 2xl:w-14 2xl:h-14 "
-          />
-          <div className="flex flex-col">
-            <p className="text-[#848BAC] font-semibold text-xs 2xl:text-base  mb-1 2xl:mb-2">
-              Welcome Back!
-            </p>
-            <h3 className=" text-lg 2xl:text-xl font-bold text-white">
-              Adrew Smith
-            </h3>
+        {!isCollapsed && (
+          <div className="flex items-center gap-3">
+            <Image
+              src="/images/hello.png"
+              alt="Client"
+              width={46}
+              height={46}
+              className="2xl:w-14 2xl:h-14"
+            />
+            <div className="flex flex-col">
+              <p className="text-[#848BAC] font-semibold text-xs 2xl:text-base mb-1 2xl:mb-2">
+                Welcome Back!
+              </p>
+              <h3 className="text-lg 2xl:text-xl font-bold text-white">
+                Adrew Smith
+              </h3>
+            </div>
           </div>
-        </div>
+        )}
 
-        <div className="flex flex-col gap-0.5 ">
+        <div className="flex flex-col gap-0.5">
           {navlinks.map((link, index) => (
             <Link
               key={index}
               href={link.link}
-              className={`inline-flex font-bold uppercase text-sm  2xl:text-lg 
-        ${
-          pathname === link.link
-            ? "text-white inner-left-shadow p-3 2xl:p-4 bg-[#181926] rounded-2xl "
-            : "text-[#848BAC] px-3 2xl:px-4 p-2"
-        }
-        
-        items-center gap-2`}
+              className={`inline-flex  font-bold uppercase text-sm  2xl:text-lg 
+                ${
+                  pathname === link.link
+                    ? "text-white inner-left-shadow p-3 2xl:p-4 bg-[#181926] rounded-2xl"
+                    : "text-[#848BAC] px-3 2xl:px-4 p-2 rounded-lg hover:bg-[#27283197]"
+                }
+                items-center gap-2 ${isCollapsed ? "justify-center" : ""}`}
             >
               <Image
                 src={
                   pathname === link.link ? link.icons[0].src : link.icons[1].src
                 }
-                alt="Lex Lynne Fitness"
+                alt="icon"
                 width={16}
-                className={` 2xl:w-[20px] `}
+                className="2xl:w-[20px]"
                 height={16}
                 priority
               />
-              {link.title}
+              {!isCollapsed && link.title}
             </Link>
           ))}
         </div>
-        <div className=" absolute bottom-5 flex-col flex">
+        <div className="absolute bottom-5 flex-col flex">
           <Link
             href={"/help"}
-            className={`inline-flex font-bold text-sm  2xl:text-lg 
-            text-[#848BAC] px-3 2xl:px-4 p-2
-        items-center gap-2`}
+            className={`inline-flex font-bold text-sm 2xl:text-lg text-[#848BAC] px-3 2xl:px-4 p-2 items-center gap-2`}
           >
             <Image
               src={"/icons/help.svg"}
-              alt="Lex Lynne Fitness"
+              alt="Help"
               width={16}
-              className={` 2xl:w-[20px] `}
+              className="2xl:w-[20px]"
               height={16}
               priority
             />
-            HELP
+            {!isCollapsed && <p>HELP</p>}
           </Link>
           <button
-            className={`inline-flex uppercase font-bold text-sm  2xl:text-lg 
-            text-[#848BAC] px-3 2xl:px-4 p-2
-        items-center gap-2`}
+            onClick={handleCollapseToggle}
+            className={`inline-flex uppercase font-bold text-sm 2xl:text-lg text-[#848BAC] px-3 2xl:px-4 p-2 items-center gap-2`}
           >
             <Image
               src={"/icons/collapse.svg"}
-              alt="Lex Lynne Fitness"
+              alt={isCollapsed ? "Expand" : "Collapse"}
               width={16}
-              className={` 2xl:w-[20px] `}
+              className={` ${
+                isCollapsed && " rotate-180"
+              } transition-all duration-500 2xl:w-[20px]`}
               height={16}
               priority
             />
-            collapse
+            {!isCollapsed && "Collapse"}
           </button>
         </div>
       </div>
-      {/* Movbile Sidebar */}
-
-      {/* Mobile Sidebar end */}
     </>
   );
 };
