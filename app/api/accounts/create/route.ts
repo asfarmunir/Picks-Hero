374dfb29c6b4ddc39c9c34d24d08f445dc611c3a
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-09-30 00:35:29 +0500

    update create account api

--- app/api/accounts/create/route.ts
+++ app/api/accounts/create/route.ts
@@ -10,9 +10,10 @@ import {
   LEVEL_2_TARGET,
   REFER_COMMISSIONS,
 } from "@/lib/constants";
+import { dateToFullCronString } from "@/lib/utils";
 
 function generateInvoice(account: any, newAccount: any, userId: any) {
-  // strip $ from accounPrice
+  // Strip $ from accountPrice
   const accountPrice = account.accountPrice.replace("$", "");
 
   const invoice = {
@@ -87,6 +88,14 @@ async function handleReferralCommission(user: any, account: any, accountInvoice:
   });
 }
 
+type cronJobTypes = "objectiveMin" | "objectiveMax" | "inactivity";
+interface cronJob {
+  jobName: string;
+  time: string;
+  type: cronJobTypes;
+  accountId: string;
+}
+
 export async function POST(req: NextRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const { account, billingDetails, card, userId } = await req.json();
@@ -102,7 +111,7 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
         );
       }
 
-      // find user
+      // Find user
       const user = await prisma.user.findFirst({
         where: {
           email: session.user?.email,
@@ -116,82 +125,128 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
         account.accountSize.replace("K", "000")
       );
 
-      // Create a new account linked to the user
-      const newAccount = await prisma.account.create({
-        data: {
-          accountSize: account.accountSize,
-          accountType: account.accountType,
-          status: account.status,
-          balance: startingBalance,
-          accountNumber: generateCustomId(),
-          userId: user.id,
-        },
-      });
-
-      // save billing address
-      const billingAddress = await prisma.billingAddress.create({
-        data: {
-          address: billingDetails.address,
-          city: billingDetails.city,
-          country: billingDetails.country,
-          email: billingDetails.email,
-          firstName: billingDetails.firstName,
-          lastName: billingDetails.lastName,
-          phone: billingDetails.phone,
-          zipCode: billingDetails.postalCode,
-          state: billingDetails.state,
-          accountId: newAccount.id,
-        },
-      });
-
-      console.log("Billing address saved!");
-
-      // save payment card
-
-      const existingCard = await prisma.paymentCard.findFirst({
-        where: {
-          cardNumber: card.cardNumber,
-          userId: user.id
-        },
-      });
-
-      let paymentCardId = existingCard?.id;
-      if (!existingCard) {
-        const paymentCard = await prisma.paymentCard.create({
+      // Use transaction to create account, billing address, and payment card
+      const [newAccount, billingAddress, paymentCard, accountInvoice] = await prisma.$transaction(async (tx) => {
+        // Create a new account linked to the user
+        const newAccount = await tx.account.create({
           data: {
-            cardNumber: card.cardNumber,
-            cardExpiry: card.cardExpiry,
-            zipCode: card.zipCode,
-            cardCvv: card.cardCvv,
-            country: card.country,
-            userId: userId,
+            accountSize: account.accountSize,
+            accountType: account.accountType,
+            status: account.status,
+            balance: startingBalance,
+            accountNumber: generateCustomId(),
+            userId: user.id,
+            minBetPeriod: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 7 days from now
+            maxBetPeriod: new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+          },
+        });
+
+        // Save billing address
+        const billingAddress = await tx.billingAddress.create({
+          data: {
+            address: billingDetails.address,
+            city: billingDetails.city,
+            country: billingDetails.country,
+            email: billingDetails.email,
+            firstName: billingDetails.firstName,
+            lastName: billingDetails.lastName,
+            phone: billingDetails.phone,
+            zipCode: billingDetails.postalCode,
+            state: billingDetails.state,
             accountId: newAccount.id,
           },
         });
 
-        paymentCardId = paymentCard.id;
-      }
+        console.log("Billing address saved!");
 
-      if(!paymentCardId) {
-        return NextResponse.json({ error: "Failed to save payment card" }, { status: 500 });
-      }
+        // Save payment card
+        const existingCard = await tx.paymentCard.findFirst({
+          where: {
+            cardNumber: card.cardNumber,
+            userId: user.id,
+          },
+        });
 
-      const accountInvoice = await prisma.accountInvoices.create({
-        data: {
-          ...generateInvoice(account, newAccount, userId),
-          paymentCardId: paymentCardId,
-        },
+        let paymentCardId = existingCard?.id;
+        if (!existingCard) {
+          const paymentCard = await tx.paymentCard.create({
+            data: {
+              cardNumber: card.cardNumber,
+              cardExpiry: card.cardExpiry,
+              zipCode: card.zipCode,
+              cardCvv: card.cardCvv,
+              country: card.country,
+              userId: userId,
+              accountId: newAccount.id,
+            },
+          });
+
+          paymentCardId = paymentCard.id;
+        }
+
+        if (!paymentCardId) {
+          throw new Error("Failed to save payment card");
+        }
+
+        // Create account invoice
+        const accountInvoice = await tx.accountInvoices.create({
+          data: {
+            ...generateInvoice(account, newAccount, userId),
+            paymentCardId: paymentCardId,
+          },
+        });
+
+        // set CRON job for minimum Bet Period
+        const sevenday_cron_job: cronJob = {
+          jobName: `${newAccount.id}_MIN_BET_PERIOD`,
+          time: dateToFullCronString(newAccount.minBetPeriod),
+          type: "objectiveMin",
+          accountId: newAccount.id,
+        };
+        const objectiveMinJob =  await fetch(`${process.env.BG_SERVICES_URL}/add-cron-job`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(sevenday_cron_job),
+        });
+        if (!objectiveMinJob.ok) {
+          throw new Error(await objectiveMinJob.text());
+        }
+
+        // set CRON job for maximum Bet Period
+        const thirtyday_cron_job: cronJob = {
+          jobName: `${newAccount.id}_MAX_BET_PERIOD`,
+          time: dateToFullCronString(newAccount.maxBetPeriod),
+          type: "objectiveMax",
+          accountId: newAccount.id,
+        };
+        const objectiveMaxJob = await fetch(`${process.env.BG_SERVICES_URL}/add-cron-job`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(thirtyday_cron_job),
+        });
+        if (!objectiveMaxJob.ok) {
+          throw new Error(await objectiveMaxJob.text());
+        }
+
+        return [newAccount, billingAddress, paymentCardId, accountInvoice];
+      }, {
+        timeout: 10000, // 10 seconds
       });
 
-      // give commission to referrer on first purchase
+      // Give commission to referrer on first purchase
       const numberOfAccounts = await prisma.account.count({
         where: {
           userId: user.id,
         },
-      })
+      });
 
-      if(numberOfAccounts > 1)
+      if (numberOfAccounts > 1) {
         return NextResponse.json({ newAccount }, { status: 200 });
+      }
 
       await handleReferralCommission(user, account, accountInvoice);
       
